d7bc714cc22815184f424bf1400ffcdff3df3cd4
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2022-02-16 08:46:18 -0800

    create folders and files

---
+++ src/Form/Form.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+    constructor(){
+        super();
+        this.state = {
+            name: '',
+            date: '',
+            time: '',
+            number: '',
+        }
+    
+    }
+
+
+}
+
+export default Form;
---
+++ src/Reservations/Reservations.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const Reservations = () => {
+    return (
+        <h1>Hello World!</h1>
+    )
+}
+
+
+export default Reservations;
